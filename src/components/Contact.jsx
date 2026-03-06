43339d45fa5a036a29a4966579a1bc4728494a01
wkaiser21 <wyatt.kaiser@outlook.com>
2026-03-05 21:50:13 -0500

    feat - Navbar functionality complete

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -4,9 +4,28 @@ import { FaLinkedin, FaGithub } from "react-icons/fa";
 function Contact() {
   return (
     <div className="flex items-center gap-4">
-      <a href="mailto:wyatt.kaiser@outlook.com" className="hover:text-blue-400 transition"><IoIosMail /></a>
-      <a href="https://www.linkedin.com/in/wyatt-kaiser" target="_blank" rel="noopener noreferrer" className="hover:text-blue-400 transition"><FaLinkedin /></a>
-      <a href="https://github.com/wkaiser21" target="_blank" rel="noopener noreferrer" className="hover:text-blue-400 transition"><FaGithub /></a>
+      <a
+        href="mailto:wyatt.kaiser@outlook.com"
+        className="hover:text-blue-400 transition"
+      >
+        <IoIosMail />
+      </a>
+      <a
+        href="https://www.linkedin.com/in/wyatt-kaiser"
+        target="_blank"
+        rel="noopener noreferrer"
+        className="hover:text-blue-400 transition"
+      >
+        <FaLinkedin />
+      </a>
+      <a
+        href="https://github.com/wkaiser21"
+        target="_blank"
+        rel="noopener noreferrer"
+        className="hover:text-blue-400 transition"
+      >
+        <FaGithub />
+      </a>
     </div>
   );
 }
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,12 +1,30 @@
+import Contact from "./Contact";
+
 function Navbar() {
   return (
-    <div className="navbar">
-      <h1>Wyatt Kaiser</h1>
-      <a href="#home">Home</a>
-      <a href="#about">About</a>
-      <a href="#experience">Experience</a>
-      <a href="#projects">Projects</a>
-    </div>
+    <nav className="fixed top-0 left-0 w-full bg-slate-900 text-white">
+      <div className="max-w-6x1 mx-auto flex items-center justify-between px-6 py-4">
+        {/*Left Side*/}
+        <div className="flex items-center gap-8">
+          <h1 className="font-semibold text-lg">Wyatt Kaiser</h1>
+          <a href="#home" className="hover:text-blue-400 transition">
+            Home
+          </a>
+          <a href="#about" className="hover:text-blue-400 transition">
+            About
+          </a>
+          <a href="#experience" className="hover:text-blue-400 transition">
+            Experience
+          </a>
+          <a href="#projects" className="hover:text-blue-400 transition">
+            Projects
+          </a>
+        </div>
+
+        {/*Right Side*/}
+        <Contact />
+      </div>
+    </nav>
   );
 }
 
